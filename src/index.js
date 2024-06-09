3edaf3b35e2c464036d66559138e7de1f8e929f2
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-06-09 13:23:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ cloudinary.config({
 
 const app = express();
 app.use(express.json());
-app.use(cors({ "Access-Control-Allow-Origin": "*", origin: true }));
+app.use(cors({origin: '*' }));
 
 app.get("/authorized", function (req, res) {
   res.send("Secured Resource");
